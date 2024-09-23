5c484d3e2f78ade2b8cf821cb255164762e026fb
AdamBrown04 <adambrown2004@outlook.com>
2024-09-23 11:10:27 +0100

    Add project files.

---
+++ Y2WK1-EssentailTasks/Program.cs
@@ -0,0 +1,10 @@
+﻿while (true)
+{
+    Console.WriteLine("Which program would you like to run? \n1)What is next");
+    int option = Convert.ToInt32(Console.ReadLine());
+
+    if (option == 1)
+    {
+
+    }
+}
