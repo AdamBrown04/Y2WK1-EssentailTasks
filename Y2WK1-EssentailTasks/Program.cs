541badd5af8eaa90f76348fcb7228080473b8aea
AdamBrown04 <adambrown2004@outlook.com>
2024-09-23 11:20:25 +0100

    completed the first task

--- Y2WK1-EssentailTasks/Program.cs
+++ Y2WK1-EssentailTasks/Program.cs
@@ -6,6 +6,31 @@
     if (option == 1)
     {
         Console.WriteLine("What are you currently doing?");
-        string currentAction = Console.ReadLine();
+        string currentAction = Console.ReadLine().ToLower();
+        WhatIsNext(currentAction);
     }
+}
+
+
+static void WhatIsNext(string currentAction)
+{
+    switch (currentAction) 
+    {
+        case "shower":
+            Console.WriteLine("have breakfast");
+        break;
+        case "have breakfast":
+            Console.WriteLine("program");
+            break;
+        case "program":
+            Console.WriteLine("play games");
+            break;
+        case "play games":
+            Console.WriteLine("make dinner");
+            break;
+        case "make dinner":
+            Console.WriteLine("shower");
+            break;
+    }
+
 }
